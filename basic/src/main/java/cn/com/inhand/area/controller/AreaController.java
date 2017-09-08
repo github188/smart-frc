221d245e562b29131593e8758a0b7212ecf5c66c
fenghl <fenghl@inhand.com.cn>
2017-09-08 10:59:28 +0800

    修改区域

--- basic/src/main/java/cn/com/inhand/area/controller/AreaController.java
+++ basic/src/main/java/cn/com/inhand/area/controller/AreaController.java
@@ -6,7 +6,11 @@ package cn.com.inhand.area.controller;
 
 import cn.com.inhand.area.dao.AreaDao;
 import cn.com.inhand.common.dto.AreaBean;
+import cn.com.inhand.common.dto.BasicResultDTO;
 import cn.com.inhand.common.dto.OnlyResultDTO;
+import cn.com.inhand.common.smart.model.Goods;
+import cn.com.inhand.common.util.DateUtils;
+import cn.com.inhand.smart.formulacar.model.Area;
 import java.util.List;
 import javax.validation.Valid;
 import org.bson.types.ObjectId;
@@ -27,11 +31,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("api/basic")
 public class AreaController {
-    
+
     @Autowired
     private AreaDao areaDao;
-    
-    @RequestMapping(value = "/area}", method = RequestMethod.POST)
+
+    @RequestMapping(value = "/area/list", method = RequestMethod.GET)
+    public @ResponseBody
+    Object getAllArea(@RequestParam("access_token") String accessToken,
+            @RequestParam(required = false, defaultValue = "30") int limit,
+            @RequestParam(required = false, defaultValue = "0") int cursor,
+            @RequestHeader(value = "X-API-OID", required = false) ObjectId xOId,
+            @RequestHeader(value = "X-API-USERNAME", required = false) String xUsername,
+            @RequestHeader(value = "X-API-IP", required = false) String xIp,
+            @RequestHeader(value = "X-API-UID", required = false) ObjectId xUId,
+            @RequestHeader(value = "X-API-ACLS", required = false) List<ObjectId> xAcls) {
+
+        long total = areaDao.getCount(xOId, null);
+        List<Area> areaList = areaDao.findAreaByParam(xOId, null, cursor, limit);
+        return new BasicResultDTO(total, cursor, limit, areaList);
+    }
+
+    @RequestMapping(value = "/area", method = RequestMethod.POST)
     public @ResponseBody
     Object saveAutomat(@RequestParam(value = "access_token", required = true) String access_token,
             @RequestHeader(value = "X-API-OID", required = false) ObjectId xOId,
@@ -40,13 +60,20 @@ public class AreaController {
             @RequestHeader(value = "X-API-UID", required = false) ObjectId xUId,
             @RequestHeader(value = "X-API-ACLS", required = false) List<ObjectId> xAcls,
             @Valid @RequestBody AreaBean areaBean) {
-         
-        
-         OnlyResultDTO result = new OnlyResultDTO();
-         return result;
+        Area area = new Area();
+        area.setAreaNum(areaBean.getAreaNum());
+        area.setCreateTime(DateUtils.getUTC());
+        area.setDesc(areaBean.getDesc());
+        area.setName(areaBean.getName());
+        area.setPhone(areaBean.getPhone());
+        area.setUpdateTime(DateUtils.getUTC());
+        area.setOid(xOId);
+        areaDao.createArea(xOId, area);
+        OnlyResultDTO result = new OnlyResultDTO();
+        result.setResult("OK");
+        return result;
     }
-    
-    
+
     @RequestMapping(value = "/area/{id}", method = RequestMethod.PUT)
     public @ResponseBody
     Object updateAutomat(@RequestParam(value = "access_token", required = true) String access_token,
@@ -57,13 +84,36 @@ public class AreaController {
             @RequestHeader(value = "X-API-ACLS", required = false) List<ObjectId> xAcls,
             @PathVariable ObjectId id,
             @Valid @RequestBody AreaBean areaBean) {
-         
-        
-         OnlyResultDTO result = new OnlyResultDTO();
-         return result;
+        Area area = areaDao.findAreaById(xOId, id);
+        if (area != null) {
+            area.setAreaNum(areaBean.getAreaNum());
+            area.setDesc(areaBean.getDesc());
+            area.setName(areaBean.getName());
+            area.setPhone(areaBean.getPhone());
+            area.setUpdateTime(DateUtils.getUTC());
+            areaDao.updateArea(xOId, area);
+        }
+        OnlyResultDTO result = new OnlyResultDTO();
+        result.setResult("OK");
+        return result;
     }
-    
-    @RequestMapping(value = "/area", method = RequestMethod.DELETE)
+
+    @RequestMapping(value = "/area/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    Object findArea(@RequestParam(value = "access_token", required = true) String access_token,
+            @RequestHeader(value = "X-API-OID", required = false) ObjectId xOId,
+            @RequestHeader(value = "X-API-USERNAME", required = false) String xUsername,
+            @RequestHeader(value = "X-API-IP", required = false) String xIp,
+            @RequestHeader(value = "X-API-UID", required = false) ObjectId xUId,
+            @RequestHeader(value = "X-API-ACLS", required = false) List<ObjectId> xAcls,
+            @PathVariable ObjectId id) {
+        Area area = areaDao.findAreaById(xOId, id);
+        OnlyResultDTO result = new OnlyResultDTO();
+        result.setResult(area);
+        return result;
+    }
+
+    @RequestMapping(value = "/areaDelBatch", method = RequestMethod.POST)
     public @ResponseBody
     Object deleteAutomat(@RequestParam(value = "access_token", required = true) String access_token,
             @RequestHeader(value = "X-API-OID", required = false) ObjectId xOId,
@@ -71,12 +121,12 @@ public class AreaController {
             @RequestHeader(value = "X-API-IP", required = false) String xIp,
             @RequestHeader(value = "X-API-UID", required = false) ObjectId xUId,
             @RequestHeader(value = "X-API-ACLS", required = false) List<ObjectId> xAcls,
-            @PathVariable ObjectId id,
-            @Valid @RequestBody AreaBean areaBean) {
-         
+            @RequestParam(value = "ids", required = true) String ids) {
         
-         OnlyResultDTO result = new OnlyResultDTO();
-         return result;
+        String[] idsArr_ = ids.split(",");
+        areaDao.deleteByIds(xOId, idsArr_);
+        OnlyResultDTO result = new OnlyResultDTO();
+        result.setResult("OK");
+        return result;
     }
-    
 }
--- basic/src/main/java/cn/com/inhand/area/dao/AreaDao.java
+++ basic/src/main/java/cn/com/inhand/area/dao/AreaDao.java
@@ -4,6 +4,7 @@
  */
 package cn.com.inhand.area.dao;
 
+import cn.com.inhand.common.dto.AreaBean;
 import cn.com.inhand.smart.formulacar.model.Area;
 import java.util.List;
 import java.util.Map;
@@ -19,7 +20,13 @@ public interface AreaDao {
     
     public void updateArea(ObjectId oid,Area area);
     
+    public Area findAreaById(ObjectId oid,ObjectId id);
+    
     public List<Area> findAreaByParam(ObjectId oid,Map<String,Object> params,int skip, int limit);
     
     public void deleteArea(ObjectId oid,String[] idsArr);
+    
+    public Long getCount(ObjectId oid,AreaBean queryBean);
+    
+    public void deleteByIds(ObjectId oId, String[] idsArr);
 }
