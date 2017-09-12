9d3fe8ff92aacc34f4c7d2f7aceb87573d71acb1
fenghl <fenghl@inhand.com.cn>
2017-09-12 15:11:41 +0800

    修改

--- frccentra/src/main/java/cn/com/inhand/centra/device/controller/DeviceRfidOauthController.java
+++ frccentra/src/main/java/cn/com/inhand/centra/device/controller/DeviceRfidOauthController.java
@@ -6,12 +6,15 @@ package cn.com.inhand.centra.device.controller;
 
 import cn.com.inhand.centra.device.dao.CardsDao;
 import cn.com.inhand.centra.device.dao.MemberDao;
+import cn.com.inhand.centra.device.dao.SiteDAO;
 import cn.com.inhand.centra.device.handle.OauthConfigHandler;
 import cn.com.inhand.common.exception.ErrorCode;
 import cn.com.inhand.common.exception.ErrorCodeException;
 import cn.com.inhand.common.model.DeviceKey;
 import cn.com.inhand.smart.formulacar.model.Cards;
+import cn.com.inhand.smart.formulacar.model.Device;
 import cn.com.inhand.smart.formulacar.model.Member;
+import cn.com.inhand.smart.formulacar.model.Site;
 import java.util.HashMap;
 import java.util.Map;
 import org.slf4j.Logger;
@@ -38,18 +41,28 @@ public class DeviceRfidOauthController {
     private CardsDao cardDao;
     @Autowired
     private MemberDao memberDao;
+    @Autowired
+    private SiteDAO siteDao;
 
     @RequestMapping(value = "/rfid/oauth", method = RequestMethod.GET)
     public synchronized @ResponseBody
     Object syncAssetStatus(@RequestParam(value = "access_token", required = true) String access_token,
-            @RequestParam(value = "rfid", required = true) String rfid) {
-        logger.info("Device Rfid oauth rfid is {}", rfid);
+            @RequestParam(value = "rfid", required = true) String rfid,
+            @RequestParam(value = "assetId", required = true) String assetId) {
+        logger.info("Device Rfid oauth rfid is {} assetId is {}", rfid,assetId);
 
         DeviceKey key = oauthHandler.verifyDeviceKey(access_token);//deviceKeyDAO.getDeviceKeyByKey(access_token);
         if (key == null) {
             throw new ErrorCodeException(ErrorCode.DEVICE_KEY_NOT_EXISIT, access_token);
         }
 
+        Device device = siteDao.getDeviceByAssetId(key.getOid(), assetId);
+        if(device != null && device.getSiteId() != null && !device.getSiteId().toString().equals("")){
+            Site site = siteDao.getSiteById(key.getOid(), device.getSiteId());
+            if(site != null){
+            }
+        }
+        
         Cards card = cardDao.findCardByRfid(key.getOid(), rfid);
         Map<String, String> content = new HashMap<String, String>();
         if (card != null) {
